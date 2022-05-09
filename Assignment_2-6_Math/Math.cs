7fc4886b99001a4d2f9d8a1335cc9e9eb0f2db39
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-05-09 10:52:22 -0600

    feat: Completed script for A2-6

---
+++ Assignment_2-6_Math/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assignment_2_6_Math
+{
+    class Math
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of given circle: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            int diameter = 2 * radius;
+            double circumference = 2 * 3.14159 * radius;
+            double area = 3.14159 * (radius * radius); 
+            
+            Console.WriteLine($"\ndiameter = {diameter}\ncircumference = {circumference}\narea = {area}");
+        }
+    }
+}
